ba1432891aeaabe9e0c947669e0a3986d264c2af
simoesferrao-kevin-unilu <114000966+simoesferrao-kevin-unilu@users.noreply.github.com>
2023-11-26 18:12:33 +0100

    Add files via upload

---
+++ Project JUnit CI/src/main/java/uni/lu/Calculator.java
@@ -0,0 +1,12 @@
+package uni.lu;
+
+/**
+ * A simple calculator that does the addition between two given numbers.
+ */
+public class Calculator {
+
+    // Calculates the addition of two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
